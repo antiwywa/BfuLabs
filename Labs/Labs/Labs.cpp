dc0bfeca276d2ac33e03cc3358c9d1dee47e8b4a
antiwywa <sanyupamosh2007@gmail.com>
2024-09-29 20:10:07 +0200

    разделение кода на методы

--- Labs/Labs/Labs.cpp
+++ Labs/Labs/Labs.cpp
@@ -1,14 +1,9 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 using namespace std;
 
-int main()
+vector<int> NumberToBit(uint16_t numberToBit)
 {
-	setlocale(LC_ALL, "Russian");
-	uint16_t numberToBit = 0;
-	cout << "Введите значение для обработки:\n";
-	cin >> numberToBit;
 	vector<int> numberInBits = {};
 	while (numberToBit > 0)
 	{
@@ -19,12 +14,36 @@ int main()
 		numberToBit /= 2;
 	}
 	reverse(numberInBits.begin(), numberInBits.end());
+
+	return numberInBits;
+}
+
+vector<int> IndexToNewBit(int index, int newBit, vector<int> numberInBits)
+{
+	numberInBits[index] = newBit;
+
+	return numberInBits;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	uint16_t numberToBit = 0;
+	cout << "Введите значение для обработки:\n";
+	cin >> numberToBit;
+	auto numberInBits = NumberToBit(numberToBit);
+	for (int i = 0;i < numberInBits.size(); i++)
+		cout << numberInBits[i];
 	int index, newBit = 0;
 	cout << "Введите индекс для изменения бита числа:\n";
 	cin >> index;
 	cout << "Введите значение нового бита:\n";
 	cin >> newBit;
-	numberInBits[index] = newBit;
+	numberInBits = IndexToNewBit(index, newBit, numberInBits);
 	for (int i = 0;i < numberInBits.size(); i++)
 		cout << numberInBits[i];
+
+
+	return 0;
 }
+
